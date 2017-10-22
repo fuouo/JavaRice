0233476504e8858cd9ea1b4e4d3e4d70b1a28d78
fuouo <fuouokun@gmail.com>
2017-10-22 18:31:11 +0800

    Fixed selection bug in erro table

--- src/view/consolepanels/ErrorPanel.java
+++ src/view/consolepanels/ErrorPanel.java
@@ -45,19 +45,26 @@ public class ErrorPanel extends Panel {
 				}
 			));
 		
-		
-		//errorTable.getColumnModel().getColumn(2).setPreferredWidth(1020);
 		errorTable.getColumnModel().getColumn(0).setPreferredWidth(130);
 		errorTable.getColumnModel().getColumn(1).setPreferredWidth(100);
 		errorTable.getColumnModel().getColumn(3).setPreferredWidth(1020);
 		JScrollPane scrollPane = new JScrollPane(errorTable);
+		
+		errorTable.setCellSelectionEnabled(false);
+		errorTable.setColumnSelectionAllowed(false);
+		errorTable.setRowSelectionAllowed(true);
 			
 		
 		errorTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
 
 			@Override
 			public void valueChanged(ListSelectionEvent arg0) {
+				if(errorTable.getSelectedRow() <= -1){
+					return;
+				}
 				int line = Integer.parseInt(errorTable.getValueAt(errorTable.getSelectedRow(), 2).toString());
+				if(line < 0)
+					return;
 				System.out.println("LINE: " + line);
 				IDEController cntrl = (IDEController) controller;
 				cntrl.goToLine(line);
@@ -91,6 +98,8 @@ public class ErrorPanel extends Panel {
 		if(list==null)
 			return;
 		removeAllRow();
+		errorTable.revalidate();
+		
 		for(int i=0; i<list.size(); i++){
 			Error error = ((Error)list.get(i));
 			addRow(
