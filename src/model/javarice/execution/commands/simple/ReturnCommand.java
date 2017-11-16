7d53599ca0dbaf973d51cffb2372222925896019
mavicreccion <mavicreccion@users.noreply.github.com>
2017-11-16 13:41:05 +0800

    added undeclaredchecker in return command

--- src/model/javarice/execution/commands/simple/ReturnCommand.java
+++ src/model/javarice/execution/commands/simple/ReturnCommand.java
@@ -2,6 +2,7 @@ package model.javarice.execution.commands.simple;
 
 import model.javarice.JavaRiceParser.ExpressionContext;
 import model.javarice.error.errorcheckers.TypeErrorChecker;
+import model.javarice.error.errorcheckers.UndeclaredChecker;
 import model.javarice.execution.commands.ICommand;
 import model.javarice.execution.commands.evaluation.EvaluationCommand;
 import model.javarice.semantics.representations.JavaRiceFunction;
@@ -16,8 +17,9 @@ public class ReturnCommand implements ICommand {
 	public ReturnCommand(ExpressionContext expressionContext, JavaRiceFunction assignedJavaRiceFunction) {
 		this.expressionContext = expressionContext;
 		this.assignedJavaRiceFunction = assignedJavaRiceFunction;
-		
-		// undeclared checker here
+
+		UndeclaredChecker undeclaredChecker = new UndeclaredChecker(this.expressionContext);
+		undeclaredChecker.verify();
 		
 		JavaRiceValue javaRiceValue = this.assignedJavaRiceFunction.getReturnValue();
 		TypeErrorChecker typeChecker = new TypeErrorChecker(javaRiceValue, this.expressionContext);
